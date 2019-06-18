69cc35c229f7dcc29708d7cf8a2f88472347d73d
fguibon <fguibon@excilys.com>
2019-06-18 16:53:20 +0200

    service implementation done

--- src/app/company.service.ts
+++ src/app/company.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { CompanyModel } from './company-model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  private companiesUrl = 'https://mock-cdb.firebaseio.com/companies';  // URL to web api
+
+  constructor(private http: HttpClient) { }
+
+  createCompany(company : CompanyModel): Observable<CompanyModel> {
+    return this.http.post<CompanyModel>(this.companiesUrl+'.json',company);
+  }
+
+  getCompanies(): Observable<CompanyModel[]> {
+    return this.http.get<CompanyModel[]>(this.companiesUrl+'.json');
+  }
+
+  getCompany(id : string) : Observable<CompanyModel> {
+    return this.http.get<CompanyModel>(this.companiesUrl+'/'+id+'.json');
+  }
+
+  updateCompany(company : CompanyModel) : Observable<any> {
+    return this.http.put(this.companiesUrl+'/'+company.id+'.json',company);
+  }
+
+  deleteCompany(id: string): Observable<CompanyModel> {
+    return this.http.delete<CompanyModel>(this.companiesUrl+'/'+id+'.json');
+  }
+
 }
