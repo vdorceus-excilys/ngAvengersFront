9c05b81dacdf7319b07669c5ce0f2ab531617464
fguibon <fguibon@excilys.com>
2019-06-18 18:03:02 +0200

    Modified test class

--- src/app/company.service.spec.ts
+++ src/app/company.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CompanyService } from './company.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CompanyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+    providers: [
+      { useValue: 'https://mock-cdb.firebaseio.com/companies' },
+      CompanyService
+    ]
+  }));
 
   it('should be created', () => {
     const service: CompanyService = TestBed.get(CompanyService);
