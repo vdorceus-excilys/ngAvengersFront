3aade50e66c6dd4a44d5a14121f70f6492b8afbd
Valjery W Dorceus <dwelinghton@excilys.com>
2019-06-19 17:15:34 +0200

    fixing code smells

--- src/app/list-company/list-company.component.ts
+++ src/app/list-company/list-company.component.ts
@@ -30,25 +30,18 @@ export class ListCompanyComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
 
-  constructor(private companyService: CompanyService,private changeDetector: ChangeDetectorRef) {     
-    
-  }
+  constructor(private companyService: CompanyService,private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.companyService.getCompanies().subscribe(
-      data => {
-        console.log(data);
-        this.refresh(data);
-      }
+      data => { this.refresh(data); }
     )
   }
 
   refresh(data){
     this.dataSource = new MatTableDataSource(data);
-    //this.dataSource = data;
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    
     this.changeDetector.detectChanges();
   }
 
@@ -61,22 +54,3 @@ export class ListCompanyComponent implements OnInit {
 
 }
 
-/** Builds and returns a new User. */
-function createCompany(id: number): CompanyDTO{
-  let myCompany = {
-    id: id.toString(),
-    name: "name="+id.toString()
-  } ;
-  console.log(myCompany);
-  return myCompany;
-}
-
-/** Builds and returns a new User. */
-function createCompany(id: number): CompanyDTO{
-  let myCompany = {
-    id: id.toString(),
-    name: "name="+id.toString()
-  } ;
-  console.log(myCompany);
-  return myCompany;
-}
