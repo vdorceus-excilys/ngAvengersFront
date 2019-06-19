fae6e147379e075e7ca47e0b961fbd12a1e80523
NajibSlassi <najibslassi1996@gmail.com>
2019-06-19 17:01:11 +0200

    service computer code cleaned according to tanguys directions

--- src/app/computer.service.ts
+++ src/app/computer.service.ts
@@ -2,12 +2,10 @@ import { Injectable } from '@angular/core';
 import {ComputerModel} from '../app/computer-model';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
-
 
 @Injectable({
   providedIn: 'root'
@@ -19,76 +17,32 @@ export class ComputerService {
   constructor(private http: HttpClient) { }
 
   getComputers(): Observable<ComputerModel[]> {
-    return this.http.get<ComputerModel[]>(this.computersUrl).pipe(
-      tap(_ => this.log('fetched computers')),
-      catchError(this.handleError<ComputerModel[]>('getComputers', []))
-    );
+    return this.http.get<ComputerModel[]>(this.computersUrl);
   }
+
   getComputer(id: number): Observable<ComputerModel> {
-    const url = this.computersUrl + id;
-    return this.http.get<ComputerModel>(url);
+    return this.http.get<ComputerModel>(this.computersUrl + id);
   }
 
   /* GET computers whose name contains search term */
-  searchComputer(term: string): Observable<ComputerModel[]> {
-    if (!term.trim()) {
-      // if not search term, return empty computer array.
-      return of([]);
-    }
-    return this.http.get<ComputerModel[]>(`${this.computersUrl}/?name=${term}`).pipe(
-      tap(_ => this.log(`found computers matching "${term}"`)),
-      catchError(this.handleError<ComputerModel[]>('searchComputer', []))
-    );
+  searchComputer(search: string): Observable<ComputerModel[]> {
+    return this.http.get<ComputerModel[]>(`${this.computersUrl}/?name=${search}`);
   }
 
   /** POST: add a new computer to the server */
   addComputer(computer: ComputerModel): Observable<ComputerModel> {
-    return this.http.post<ComputerModel>(this.computersUrl, computer, httpOptions).pipe(
-      tap((newComputer: ComputerModel) => this.log(`added computer w/ name=${newComputer.name}`)),
-      catchError(this.handleError<ComputerModel>('addComputer'))
-    );
+    return this.http.post<ComputerModel>(this.computersUrl, computer, httpOptions);
   }
+
   /** DELETE: delete the computer from the server */
   deleteComputer(computer: ComputerModel | number): Observable<ComputerModel> {
     const id = typeof computer === 'number' ? computer : computer.id;
     const url = this.computersUrl + id;
 
-    return this.http.delete<ComputerModel>(url, httpOptions).pipe(
-      tap(_ => this.log(`deleted computer id=${id}`)),
-      catchError(this.handleError<ComputerModel>('deleteComputer'))
-    );
+    return this.http.delete<ComputerModel>(url, httpOptions);
   }
   /** PUT: update the computer on the server */
   updateComputer(computer: ComputerModel): Observable<any> {
-    return this.http.put(this.computersUrl + computer.id, computer, httpOptions).pipe(
-      tap(_ => this.log(`updated computer id=${computer.id}`)),
-      catchError(this.handleError<any>('updateComputer'))
-    );
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  /** Log a message in console with the MessageService */
-  private log(message: string) {
-    console.log(`ComputerService: ${message}`);
+    return this.http.put(this.computersUrl + computer.id, computer, httpOptions);
   }
 }
-
