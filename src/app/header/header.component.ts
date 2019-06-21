a6cc8bdba40fa9200352b4ea680b85d40176306f
marezes <marezes.mikearezes@msn.com>
2019-06-21 10:44:33 +0200

    Modification of header translation and a button to change languages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  languages = this.translate.getLangs();
+  currentLanguage = this.translate.currentLang;
+
+  constructor(private translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+  }
 
   ngOnInit() {
   }
 
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
