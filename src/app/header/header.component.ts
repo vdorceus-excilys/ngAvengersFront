e82d16bd1d01a1cb1239b613d5b1b1a462b5cb7f
marezes <marezes.mikearezes@msn.com>
2019-06-24 17:31:27 +0200

    Styling of language button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { Router } from '@angular/router';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +13,7 @@ export class HeaderComponent implements OnInit {
   languages = this.translate.getLangs();
   currentLanguage = this.translate.currentLang;
   active = 'computer';
+  paginator: MatPaginatorIntl;
 
   constructor(private translate: TranslateService, private router: Router) {
     translate.addLangs(['en', 'es', 'fr', 'pt']);
@@ -23,7 +25,7 @@ export class HeaderComponent implements OnInit {
   useLanguage(language: string) {
     this.translate.use(language);
   }
-  
+
   navigate(route: string) {
     this.active = route;
     this.router.navigate(['/' + route]);
