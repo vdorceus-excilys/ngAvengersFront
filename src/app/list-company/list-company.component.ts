2967589d22d13185a4787725ad7fc985b17d1d7c
fguibon <fguibon@excilys.com>
2019-06-24 18:13:04 +0200

    Selection column added and delete calls.

--- src/app/list-company/list-company.component.ts
+++ src/app/list-company/list-company.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
 import { ComputerModel } from '../computer-model';
 import { CompanyModel } from '../company-model';
 import { filter } from 'minimatch';
 import { CompanyService } from '../company.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteCompanyComponent } from '../delete-company/delete-company.component';
 
 export interface CompanyDTO {
   id: string;
@@ -24,18 +27,21 @@ export interface CompanyDTO {
 
 export class ListCompanyComponent implements OnInit {
 
-  displayedColumns: string[] = ["id","name"];
+  displayedColumns: string[] = ['select', 'id', 'name'];
   dataSource: MatTableDataSource<CompanyDTO>;
+  selection = new SelectionModel<CompanyDTO>(true, []);
   @ViewChild(MatPaginator,{static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
 
-  constructor(private companyService: CompanyService,private changeDetector: ChangeDetectorRef) { }
+  constructor(private companyService: CompanyService,
+              private changeDetector: ChangeDetectorRef,
+              public dialog: MatDialog) { }
 
   ngOnInit() {
     this.companyService.getCompanies().subscribe(
       data => { this.refresh(data); }
-    )
+    );
   }
 
   refresh(data){
@@ -47,10 +53,57 @@ export class ListCompanyComponent implements OnInit {
 
   applyFilter(filterValue: string){
     this.dataSource.filter = filterValue.trim().toLowerCase();
-    if(this.dataSource.paginator){
+    if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
 
-}
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
 
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: CompanyDTO): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
+
+  deleteDialog(): void {
+    const dialogRef = this.dialog.open(DeleteCompanyComponent, {
+      height: '400px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if(result) {
+        this.selection.selected.forEach(element => {
+          this.delete(element);
+        });
+      }
+    });
+  }
+
+  delete(element: CompanyDTO): void {
+    this.companyService.deleteCompany(element.id).subscribe(
+      () => {
+        console.log(this.dataSource.data);
+        this.dataSource.data.splice(this.dataSource.data.indexOf(element, 1));
+        console.log(this.dataSource.data);
+        this.changeDetector.detectChanges();
+      }
+    );
+  }
+
+}
