5fd424dc4df1df9c7148e24f2ef43077023fe3bd
NajibSlassi <najibslassi1996@gmail.com>
2019-06-26 17:15:40 +0200

    Some code cleaning

--- src/app/scroll-top/scroll-top.component.ts
+++ src/app/scroll-top/scroll-top.component.ts
@@ -8,18 +8,17 @@ import { DOCUMENT } from '@angular/common';
 export class ScrollTopComponent implements OnInit {
     windowScrolled: boolean;
     constructor(@Inject(DOCUMENT) private document: Document) {}
-    @HostListener("window:scroll", [])
+    @HostListener('window:scroll', [])
     onWindowScroll() {
         if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
             this.windowScrolled = true;
-        } 
-       else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+        } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
             this.windowScrolled = false;
         }
     }
     scrollToTop() {
         (function smoothscroll() {
-            var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+            const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
             if (currentScroll > 0) {
                 window.requestAnimationFrame(smoothscroll);
                 window.scrollTo(0, currentScroll - (currentScroll / 8));
