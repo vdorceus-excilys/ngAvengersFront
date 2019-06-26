c500d764f8e8258de31b2b56c0061b3bb53e03ef
NajibSlassi <najibslassi1996@gmail.com>
2019-06-26 10:54:35 +0200

    login using angular material

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,35 +10,16 @@ import { ApiService } from '../api.service';
 
 @Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
-
-    loginForm: FormGroup;
-    invalidLogin: boolean = false;
-    constructor(private formBuilder: FormBuilder, private router: Router, private apiService: ApiService) { }
-  
-    onSubmit() {
-      if (this.loginForm.invalid) {
-        return;
-      }
-      const body = new HttpParams()
-        .set('username', this.loginForm.controls.username.value)
-        .set('password', this.loginForm.controls.password.value)
-        .set('grant_type', 'password');
-  
-      this.apiService.login(body.toString()).subscribe(data => {
-        window.sessionStorage.setItem('token', JSON.stringify(data));
-        console.log(window.sessionStorage.getItem('token'));
-        this.router.navigate(['list-user']);
-      }, error => {
-          alert(error.error.error_description)
-      });
+  constructor(private router: Router) { }
+username: string;
+password: string;
+  ngOnInit() {
+  }
+  login() : void {
+    if(this.username == 'admin' && this.password == 'admin'){
+     this.router.navigate(["computer"]);
+    }else {
+      alert("Invalid credentials");
     }
-  
-    ngOnInit() {
-      window.sessionStorage.removeItem('token');
-      this.loginForm = this.formBuilder.group({
-        username: ['', Validators.compose([Validators.required])],
-        password: ['', Validators.required]
-      });
-    }
-  
+  }
   }
