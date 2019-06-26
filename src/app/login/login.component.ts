12630e431312e25196b9503f50be9ffdbb096022
Valjery W Dorceus <dwelinghton@excilys.com>
2019-06-26 16:27:08 +0200

    added login page support

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { Credentials } from '../security/security.component';
 
-import { AlertService} from 'src/app/_services/alert.service';
-import { AuthenticationService } from 'src/app/_services/authentication.service';
-import { HttpParams } from '@angular/common/http';
-import { ApiService } from '../api.service';
-
-@Component({ templateUrl: 'login.component.html' })
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.scss']
+})
 export class LoginComponent implements OnInit {
+
+  username: string;
+  password: string;
+  @Output() eventEmitter: EventEmitter<Credentials> = new EventEmitter();
+
   constructor(private router: Router) { }
-username: string;
-password: string;
+
+
   ngOnInit() {
   }
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["computer"]);
-    }else {
-      alert("Invalid credentials");
-    }
+
+  login(): void {
+    const credentials = {
+      username: this.username,
+      password: this.password
+    } as Credentials;
+
+    this.eventEmitter.emit(credentials);
   }
-  }
+
+}
